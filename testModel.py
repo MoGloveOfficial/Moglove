76475c79315640da6be15099fd763b065165b79d
MoGloveOfficial <121774521+MoGloveOfficial@users.noreply.github.com>
2023-06-20 20:53:08 +1000

    Add files via upload

--- testModel.py
+++ testModel.py
@@ -1,44 +1,25 @@
 import tensorflow as tf
 import numpy as np
-import serial
 
 # Load the saved model
 model = tf.keras.models.load_model('pattern_recognition_model')
 
-# Configure the serial connection
-ser = serial.Serial('COM4', baudrate=115200)  # Replace 'COM1' with the appropriate serial port
+# Preprocess your input
+input_data = np.array([[0.74 ,0.92 ,0.87 ,0.88 ,0.90]])  # Replace with your own input values
 
-# Continuously read and process serial input
-while True:
-    try:
-        # Read serial input
-        line = ser.readline().decode().strip()
+#input_data = (input_data - np.mean(input_data, axis=0)) / np.std(input_data, axis=1)
 
-        # Split the input into individual values
-        values = line.split(',')
+# Make predictions on the input data
+predictions = model.predict(input_data)
 
-        # Convert values to floats
-        features = np.array(values, dtype=np.float32)
+# Round the predictions to the nearest integer
+estimated_patterns = np.round(predictions).astype(int).flatten()
 
-        # Preprocess the features
-        features = (features - np.mean(features)) / np.std(features)
+# Calculate the confidence values
+confidence = np.abs(predictions - np.round(predictions)).flatten()
 
-        # Reshape the features to match the model input shape
-        features = np.reshape(features, (1, -1))
 
-        # Make predictions
-        predictions = model.predict(features)
 
-        # Calculate the estimated pattern and confidence
-        estimated_pattern = int(round(predictions[0][0]))
-        confidence = np.abs(predictions - np.round(predictions))[0][0]
-
-        # Print the estimated pattern and confidence
-        print("Estimated Pattern: {}, Confidence: {:.2f}".format(estimated_pattern, confidence))
-
-    except KeyboardInterrupt:
-        # Break the loop if Ctrl+C is pressed
-        break
-
-# Close the serial connection
-ser.close()
+# Print the estimated patterns and confidence for each prediction
+for i in range(len(predictions)):
+    print("Estimated Pattern: {}, Confidence: {:.2f}".format(estimated_patterns[i], confidence[i]))
