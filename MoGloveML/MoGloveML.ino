0ba9c683dd8eda54741b8fdf7a092d22f6839f6b
MoGloveOfficial <121774521+MoGloveOfficial@users.noreply.github.com>
2023-06-25 22:31:00 +1000

    Somewhat working transition
    Needs digital smoothing for output

--- MoGloveML/MoGloveML.ino
+++ MoGloveML/MoGloveML.ino
@@ -56,7 +56,7 @@ float upperThres = 9000.0;
 float lowerThres = 6000.0;
 
 
-//Sample at 60Hz, LPF => 10Hz
+//Sample at 100Hz, LPF => 10Hz
 
 const unsigned long calTime = 3000; //Hold for 3 seconds
 unsigned long startTime = 0;
@@ -66,7 +66,7 @@ const int res = 6;    //Resolution of quaternion output
 unsigned long previousMillis = 0;   // Previous time value
 const unsigned long interval = 10;  // Sampling time interval in milliseconds
 const float cutoff_freq   = 10.0;  //Cutoff frequency in Hz
-const float sampling_time = 0.01666666; //Sampling time in seconds.
+const float sampling_time = 0.01; //Sampling time in seconds.
 IIR::ORDER  order  = IIR::ORDER::OD3; // Order (OD1 to OD4) Higher => Smoother but more latency
 
 
@@ -76,6 +76,11 @@ Filter f1(cutoff_freq, sampling_time, order);
 Filter f2(cutoff_freq, sampling_time, order);
 Filter f3(cutoff_freq, sampling_time, order);
 Filter f4(cutoff_freq, sampling_time, order);
+
+Filter qfin00(cutoff_freq, sampling_time, order);
+Filter qfin00(cutoff_freq, sampling_time, order);
+Filter qfin00(cutoff_freq, sampling_time, order);
+
 
 BluetoothSerial SerialBT;
 
@@ -214,50 +219,87 @@ void setup() {
 void loop(){
   unsigned long currentMillis = millis();  // Get the current time
   if (currentMillis - previousMillis >= interval) {
+    Serial.print("interval =");
+    Serial.println(currentMillis - previousMillis);
     previousMillis = currentMillis;  // Update the previous time
     int val0 = analogRead(fin0);
     int val1 = analogRead(fin1);
     int val2 = analogRead(fin2);
     int val3 = analogRead(fin3);
     int val4 = analogRead(fin4);
-    
+
+    //Filter input
     float fval0 = f0.filterIn(val0);
     float fval1 = f1.filterIn(val1);
     float fval2 = f2.filterIn(val2);
     float fval3 = f3.filterIn(val3);
     float fval4 = f4.filterIn(val4);
+
+    //nm -> normalized in trained dataset
     
     fval0 = map(fval0, min0,max0,0,1000)/1000.0;
-    fval0 = (fval0-lower0)/dif0;
+    float fval0nm = (fval0-lower0)/dif0;
     fval1 = map(fval1, min1,max1,0,1000)/1000.0;
-    fval1 = (fval1-lower1)/dif1;
+    float fval1nm = (fval1-lower1)/dif1;
     fval2 = map(fval2, min2,max2,0,1000)/1000.0;
-    fval2 = (fval2-lower2)/dif2;
+    float fval2nm = (fval2-lower2)/dif2;
     fval3 = map(fval3, min3,max3,0,1000)/1000.0;
-    fval3 = (fval3-lower3)/dif3;    
+    float fval3nm = (fval3-lower3)/dif3;    
     fval4 = map(fval4, min4,max4,0,1000)/1000.0;
-    fval4 = (fval4-lower4)/dif4;
+    float fval4nm = (fval4-lower4)/dif4;
     
-    float in[5] = {fval0, fval1, fval2, fval3, fval4};
+    float in[5] = {fval0nm, fval1nm, fval2nm, fval3nm, fval4nm};
     float out = modelNN.predict(in); 
     int pred = round(out);
     float conf = 1-abs(out - pred);
-    Serial.println(pred);
-    if(0<=pred<=18){
+    Serial.print("Prediction: ");
+    Serial.print(pred);
+    Serial.print(", conf: ");
+    Serial.println(conf);
+    
+    if((0<=pred)&&(pred<=18)){
+      //Qout = (A*Qbend) + (B*conf*qtr)
+      //* => Scalar multiplication of quaternion
+      //+ => Quaternion addition
+      //A&B are tuning variables...
+      
+      //fin01 = > [1,0,0,-1]
+      //fin1 => [1,1.15,0,0]
+      //fin2 => [1,0,0,0];
+      
+      Quaternion qbend01 = {1.0f, 0.0f, 0.0f, -fval0};
+      normQuat(qbend01);
+      Quaternion qbend1 = {1.0f, 1.15f * fval1, 0.0f, 0.0f};
+      normQuat(qbend1);
+      Quaternion qbend2 = {1.0f, 1.15f * fval2, 0.0f, 0.0f};
+      normQuat(qbend2);
+      Quaternion qbend3 = {1.0f, 1.15f * fval3, 0.0f, 0.0f};
+      normQuat(qbend3);
+      Quaternion qbend4 = {1.0f, 1.15f * fval4, 0.0f, 0.0f};
+      normQuat(qbend4);
+      
       Quaternion qfin00 = {qfins00[pred][0], qfins00[pred][1], qfins00[pred][2], qfins00[pred][3]};
       Quaternion qfin01 = {qfins01[pred][0], qfins01[pred][1], qfins01[pred][2], qfins01[pred][3]};
       Quaternion qfin1 = {qfins1[pred][0], qfins1[pred][1], qfins1[pred][2], qfins1[pred][3]};
       Quaternion qfin2 = {qfins2[pred][0], qfins2[pred][1], qfins2[pred][2], qfins2[pred][3]};
       Quaternion qfin3 = {qfins3[pred][0], qfins3[pred][1], qfins3[pred][2], qfins3[pred][3]};
       Quaternion qfin4 = {qfins4[pred][0], qfins4[pred][1], qfins4[pred][2], qfins4[pred][3]};
-      //qfin00 = scaleQuat(qfin00, conf);
-      //qfin01 = scaleQuat(qfin01, conf);
-      //qfin1 = scaleQuat(qfin1, conf);
-      //qfin2 = scaleQuat(qfin2, conf);
-      //qfin3 = scaleQuat(qfin3, conf);
-      //qfin4 = scaleQuat(qfin4, conf);
       
+      qfin00 = scaleQuat(qfin00, conf);
+      qfin01 = scaleQuat(qfin01, conf);
+      qfin1 = scaleQuat(qfin1, conf);
+      qfin2 = scaleQuat(qfin2, conf);
+      qfin3 = scaleQuat(qfin3, conf);
+      qfin4 = scaleQuat(qfin4, conf);
+      
+      qfin01 = add2Quats(qfin01, qbend01);
+      qfin1 = add2Quats(qfin1,qbend1);
+      qfin2 = add2Quats(qfin2,qbend2);
+      qfin3 = add2Quats(qfin3,qbend3);
+      qfin4 = add2Quats(qfin4,qbend4);
 
+      //Filter the output
+      
       //Send the data
       //[w00,x00,y00,z00...w4,x4,y4,z4]
       SerialBT.print(String(qfin00.w,res)+", "+String(qfin00.x,res)+", "+String(qfin00.y,res)+", "+String(qfin00.z,res)+", ");
--- MoGloveML/quaternion.h
+++ MoGloveML/quaternion.h
@@ -9,6 +9,7 @@ float magQuat(const Quaternion& quat) {
   return sqrt(quat.w * quat.w + quat.x * quat.x + quat.y * quat.y + quat.z * quat.z);
 }
 
+
 void normQuat(Quaternion& quat) {
   float mag = magQuat(quat);
   quat.w /= mag;
@@ -23,6 +24,16 @@ Quaternion scaleQuat(const Quaternion& quat, float scalar) {
   result.x = quat.x * scalar;
   result.y = quat.y * scalar;
   result.z = quat.z * scalar;
+  return result;
+}
+
+Quaternion add2Quats(const Quaternion& quat1, const Quaternion& quat2){
+  Quaternion result;
+  result.w = quat1.w + quat2.w;
+  result.x = quat1.x + quat2.x;
+  result.y = quat1.y + quat2.y;
+  result.z = quat1.z + quat2.z;
+  normQuat(result);
   return result;
 }
 #endif
